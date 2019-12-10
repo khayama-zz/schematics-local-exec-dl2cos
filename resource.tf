77d884a039076f4dafed6fc2377ce4a5109ecac6
Kyouhei Hayama <khayama@users.noreply.github.com>
2019-12-11 01:21:55 +0900

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,5 +1,23 @@
-resource "null_resource" "example1" {
+resource "null_resource" "env" {
   provisioner "local-exec" {
     command = "env"
+  }
+}
+
+resource "null_resource" "ping" {
+  provisioner "local-exec" {
+    command = "ping 10.0.80.11"
+  }
+}
+
+resource "null_resource" "ping1" {
+  provisioner "local-exec" {
+    command = "ping 10.192.27.26"
+  }
+}
+
+resource "null_resource" "ping2" {
+  provisioner "local-exec" {
+    command = "ping 10.192.27.49"
   }
 }
