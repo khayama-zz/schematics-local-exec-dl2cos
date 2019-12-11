7eefe00f4060e2cfd7e1ae7a67bb26fc32bf5b05
Kyouhei Hayama <khayama@users.noreply.github.com>
2019-12-11 11:03:43 +0900

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -17,49 +17,42 @@ resource "null_resource" "whoami" {
 }
 
 resource "null_resource" "uname" {
-  depends_on = [null_resource.whoami]
   provisioner "local-exec" {
     command = "uname -a"
   }
 }
 
 resource "null_resource" "ibmcloud" {
-  depends_on = [null_resource.uname]
   provisioner "local-exec" {
     command = "ibmcloud --version"
   }
 }
 
 resource "null_resource" "oc" {
-  depends_on = [null_resource.ibmcloud]
   provisioner "local-exec" {
     command = "oc version"
   }
 }
 
 resource "null_resource" "terraform" {
-  depends_on = [null_resource.oc]
   provisioner "local-exec" {
     command = "terraform --version"
   }
 }
 
 resource "null_resource" "python2" {
-  depends_on = [null_resource.terraform]
   provisioner "local-exec" {
     command = "python2 --version"
   }
 }
 
 resource "null_resource" "python3" {
-  depends_on = [null_resource.python2]
   provisioner "local-exec" {
     command = "python3 --version"
   }
 }
 
 resource "null_resource" "command" {
-  depends_on = [null_resource.python3]
   provisioner "local-exec" {
     command = "echo $PATH | sed -e \"s/:/ /g\" | xargs ls -1 | sort -u"
   }
