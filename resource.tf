6ab80dccbef6d2bfa4fe9d4c5971ae8c0d8ff03b
Kyouhei Hayama <khayama@users.noreply.github.com>
2019-12-11 10:55:42 +0900

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -8,58 +8,61 @@ resource "null_resource" "ping" {
   provisioner "local-exec" {
     command = "ping -c 4 10.0.80.11"
   }
+  depends_on = [null_resource.env]
 }
 
 resource "null_resource" "whoami" {
   provisioner "local-exec" {
     command = "whoami"
   }
+  depends_on = [null_resource.ping]
 }
 
 resource "null_resource" "uname" {
   provisioner "local-exec" {
     command = "uname -a"
   }
+  depends_on = [null_resource.whoami]
 }
 
 resource "null_resource" "ibmcloud" {
   provisioner "local-exec" {
     command = "ibmcloud --version"
   }
-}
-
-resource "null_resource" "kubectl" {
-  provisioner "local-exec" {
-    command = "kubectl version"
-  }
+  depends_on = [null_resource.uname]
 }
 
 resource "null_resource" "oc" {
   provisioner "local-exec" {
     command = "oc version"
   }
+  depends_on = [null_resource.ibmcloud]
 }
 
 resource "null_resource" "terraform" {
   provisioner "local-exec" {
     command = "terraform --version"
   }
+  depends_on = [null_resource.oc]
 }
 
 resource "null_resource" "python2" {
   provisioner "local-exec" {
     command = "python2 --version"
   }
+  depends_on = [null_resource.terraform]
 }
 
 resource "null_resource" "python3" {
   provisioner "local-exec" {
     command = "python3 --version"
   }
+  depends_on = [null_resource.python2]
 }
 
 resource "null_resource" "command" {
   provisioner "local-exec" {
     command = "echo $PATH | sed -e \"s/:/ /g\" | xargs ls -1 | sort -u"
   }
+  depends_on = [null_resource.python3]
 }
