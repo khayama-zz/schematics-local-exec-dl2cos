cc4fc6bfe77183065cf1dda907ea634171c19136
Kyouhei Hayama <khayama@users.noreply.github.com>
2020-06-12 14:44:25 +0900

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -27,7 +27,7 @@ resource "null_resource" "curl" {
       curl --version
       object_name=$(basename ${var.url})
       echo "basename is $object_name"
-      content_type=$(curl -sI ${var.url} -w '%{content_type}\n' -o /dev/null)
+      content_type=$(curl -sI ${var.url} -w '%%{content_type}\n' -o /dev/null)
       echo "content-type is $content_type"
       #filename=$(curl -I ${var.url} | grep "Content-Disposition" | cut -d \" -f 2)
       #echo "filename is $filename"
